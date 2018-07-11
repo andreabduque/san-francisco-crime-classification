aee1ff146abac22ece86a93789163d4541d55e95
Gustavo de Oliveira Feitosa <gof@up1c10.windows.cin.ufpe.br>
2018-07-11 17:07:07 -0300

    maybe one day

---
+++ graphs.py
@@ -0,0 +1,13 @@
+# from sklearn.model_selection import GridSearchCV
+# from sklearn.pipeline import Pipeline
+# from sklearn.preprocessing import MinMaxScaler
+# from sklearn.metrics import log_loss, make_scorer
+# import pandas as pd
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 5, 0.1)
+# print(x)
+y = np.sin(x)
+plt.plot(x, x)
